6c7b7e9cfd866cff3e7225bec4b68a3783671bd2
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-09-05 23:11:20 -0300

    Propriedades são Somente Leitura

---
+++ components/SomaUm.jsx
@@ -0,0 +1,8 @@
+export default function SomaUm(props) {
+   // props.numero ++ /* DESSE JEITO NAO LE NO OUTRO PROPRIO DO ARQUIVO, SÓ COMO ESTÁ DESCRITO ABAIXO no <h1>{props.numero + 1}</h1> */
+    return (
+        <div>
+            <h1>{props.numero + 1}</h1>
+        </div>
+    )
+}
---
+++ pages/basicos/propsSomenteLeitura.jsx
@@ -0,0 +1,7 @@
+import SomaUm from "../../components/SomaUm";
+
+export default function propsSomentLeitura() {
+    return(
+        <SomaUm numero={99}/>
+    )
+}
