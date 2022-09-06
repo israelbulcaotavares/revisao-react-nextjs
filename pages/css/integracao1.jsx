d9a3787a19cb5bd2149891a531109883b70b6757
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-09-05 22:10:56 -0300

    Integração com CSS #01

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,15 @@
+/* nao posso importar aqui */
+// o import foi feito dentro de _app.js , onde é a pasta JS GLOBAL
+
+export default function integracao() {
+  return (
+    <>
+      
+      <div className="integracao1">
+        <div className="vermelha">Texto #01</div>
+        <div className="azul">Text #02</div>
+        <div className="branca">Texto #03</div>
+      </div>
+    </>
+  );
+}
