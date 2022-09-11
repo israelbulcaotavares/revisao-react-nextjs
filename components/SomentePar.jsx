e0aa0382c3eebe679b64b85c7cf310af5d59b920
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-09-11 19:47:57 -0300

    Condicional #01

---
+++ components/SomentePar.jsx
@@ -0,0 +1,23 @@
+export default function SomentePar(props ) {
+   
+    const numeroPar = props.numero %2 === 0 
+    
+
+    return (
+        <div>
+            {numeroPar ? 
+                <span>{props.numero}</span> 
+               : null }
+        </div>
+    )
+
+      // return numeroPar ? <span>{props.numero}</span> : null
+
+    // return props.numero %2 === 0 ? <span>{props.numero}</span> :   null
+    
+    // if(props.numero %2 === 0){
+    //     return <span>{props.numero}</span>
+    // }else{
+    //     return null
+    // }
+}
