c662917065f7a69a78135ff0e56bec2ec9d57ab6
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-09-13 19:33:53 -0300

    Sistema Módulo ECMAScript

---
+++ components/modulo/funcionais.jsx
@@ -0,0 +1,40 @@
+/* FUNÇÃO  SEM DEFAULT  */
+export function Comp1(){
+    return(
+        <div>
+            <h2>Comp1</h2>
+        </div>
+    )
+}
+
+
+/* FUNÇÃO COM DEFAULT  */
+//   export default function Comp2(){
+//       return(
+//           <div>
+//               <h2>Comp1</h2>
+//          </div>
+//       )
+//   }
+
+
+/* FUNÇÃO ANONIMA */
+// export default function(){ 
+//     return(
+//         <div>
+//              <h2>Comp1</h2>
+//           </div>
+//       )
+//   }
+
+// export const Comp3 = function(){
+//     return <h2>Comp2</h2>
+// }
+
+// export const Comp4 = () => {
+//     return(
+//         <div>
+//             <h1></h1>
+//         </div>
+//     )
+// }
---
+++ pages/modulo/teste.jsx
@@ -0,0 +1,9 @@
+import { Comp1 } from "../../components/modulo/funcionais";
+
+export default function teste(){
+    return (
+        <div>
+            <Comp1 />
+        </div>
+    )
+}
